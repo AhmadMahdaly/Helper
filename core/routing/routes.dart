81258b2c146d4a19f2bc4733907b6ea0aeca9faf
Ahmed Mahmoud <ahmed.mahdaly86@gmail.com>
2025-05-26 16:30:11 +0300

    Create routes.dart

---
+++ core/routing/routes.dart
@@ -0,0 +1,7 @@
+class Routes {
+  static const String loginScreen = '/loginScreen';
+  static const String registerScreen = '/registerScreen';
+  static const String forgetPasswordScreen = '/forgetPassword';
+  static const String mainlayoutScreen = '/mainLayout';
+  static const String onBoardingScreen = '/onBoardingScreen';
+}
