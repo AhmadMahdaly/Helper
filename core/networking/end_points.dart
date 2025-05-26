286d7937bb796be7e7ee4235f7de4205e5d98514
Ahmed Mahmoud <ahmed.mahdaly86@gmail.com>
2025-05-26 16:23:01 +0300

    Create end_points.dart

---
+++ core/networking/end_points.dart
@@ -0,0 +1,14 @@
+class EndPoints {
+  static const String baseUrl = 'https://absabsabs-eg.com/api/';
+  /// auth
+  static const String login = 'user/login';
+  static const String updateProfile = 'user/update';
+  static const String getProfile = 'profile';
+  static const String deleteAccount = 'user/delete';
+  static const String logout = 'logout';
+  static const String makeExcuse = 'make/excuse';
+  static const String getExeuces = 'user/excuses';
+  static const String getVacations = 'user/vacation';
+  static const String makeVacation = 'make/vacation';
+  static const String getDeparmtents = 'department';
+}
