92c61caecfd23439a4aadd5fb7215914a3ffa420
Ahmed Mahmoud <ahmed.mahdaly86@gmail.com>
2025-05-26 16:38:53 +0300

    Create constants.dart

---
+++ core/constants.dart
@@ -0,0 +1,9 @@
+
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+int mainLayoutIntitalScreenIndex = 0;
+
+String? userToken;
+final navigatorKey = GlobalKey<NavigatorState>();
+Position ? position;
