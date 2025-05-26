ec9840545c2fa7f4f4ffd4d9c0b114e4066d5188
Ahmed Mahmoud <ahmed.mahdaly86@gmail.com>
2025-05-26 16:33:27 +0300

    Create colors.dart

---
+++ core/theming/colors.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryColor = Color(0xff3085fe);
+  static const secondaryColor = Color(0xffd5d5d5);
+  static const thirdColor = Color(0xff22444d);
+  static const forthColor = Color(0xff0e0f24);
+  ///
+  static Color offerColor = const Color(0xff2aa69c).withAlfa(200);
+  static const greyColor = Color(0xffd5d5d5);
+  static const borderColor = Color(0xffd5d5d5);
+  ///
+  static const primaryTextColor = Color(0xff191c20);
+  static const secondaryTextColor = Color(0xff797b7d);
+  static const textGreyColor = Color(0xff676767);
+  ///
+  static Color successColor = Colors.green;
+  static const errorColor = Color(0xFFFF6565);
+  ///
+  static const textFormFieldColor = Color(0xffffffff);
+  static const scaffoldBackground = Color(0xffffffff);
+  static const greyColorBackground = Color(0xffF2F2F2);
+  static const newBlackColor = Color(0xff0d0c0d);
+  static const bottomNavLightThemeBackground = Colors.white;
+  static const mainContainerColor2 = Color(0xfff8f8f8);
+}
