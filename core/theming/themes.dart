edd5d2d9c3543bd9c37a24fb88fedb548d9e4176
Ahmed Mahmoud <ahmed.mahdaly86@gmail.com>
2025-05-26 16:34:35 +0300

    Create themes.dart

---
+++ core/theming/themes.dart
@@ -0,0 +1,265 @@
+import 'package:hr_app/core/theming/colors.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+abstract class Styles {
+  static final textStyle9W400 = GoogleFonts.lato(
+    textStyle: TextStyle(
+      fontSize: 9.sp,
+      color: AppColors.primaryTextColor,
+      fontWeight: FontWeight.w400,
+    ),
+  );
+  static final textStyle10W400 = GoogleFonts.lato(
+    textStyle: TextStyle(
+      fontSize: 10.sp,
+      color: AppColors.primaryTextColor,
+      fontWeight: FontWeight.w400,
+    ),
+  );
+  static final textStyle12W400 = GoogleFonts.lato(
+    textStyle: TextStyle(
+      fontSize: 12.sp,
+      color: AppColors.primaryTextColor,
+      fontWeight: FontWeight.w400,
+    ),
+  );
+  static final textStyle12W700 = GoogleFonts.lato(
+    textStyle: TextStyle(
+      fontSize: 12.sp,
+      color: AppColors.primaryTextColor,
+      fontWeight: FontWeight.w700,
+    ),
+  );
+  static final textStyle13W500 = GoogleFonts.lato(
+    textStyle: TextStyle(
+      fontSize: 13.sp,
+      color: AppColors.primaryTextColor,
+      fontWeight: FontWeight.w500,
+    ),
+  );
+
+  static final textStyle14W300 = GoogleFonts.lato(
+    textStyle: TextStyle(
+      fontSize: 14.sp,
+      color: AppColors.primaryTextColor,
+      fontWeight: FontWeight.w300,
+    ),
+  );
+  static final textStyle14W400 = GoogleFonts.lato(
+    textStyle: TextStyle(
+      fontSize: 14.sp,
+      color: AppColors.primaryTextColor,
+      fontWeight: FontWeight.w400,
+    ),
+  );
+  static final textStyle14W700 = GoogleFonts.lato(
+    textStyle: TextStyle(
+      fontSize: 12.sp,
+      fontWeight: FontWeight.w700,
+      color: AppColors.primaryTextColor,
+    ),
+  );
+  static final textStyle14W600 = GoogleFonts.lato(
+    textStyle: TextStyle(
+      fontSize: 14.sp,
+      color: AppColors.primaryTextColor,
+      fontWeight: FontWeight.w600,
+    ),
+  );
+
+  static final textStyle15W300 = GoogleFonts.lato(
+    textStyle: TextStyle(
+      fontSize: 15.sp,
+      color: AppColors.primaryTextColor,
+      fontWeight: FontWeight.w300,
+    ),
+  );
+  static final textStyle15W700 = GoogleFonts.lato(
+    textStyle: TextStyle(
+      fontSize: 15.sp,
+      color: AppColors.primaryTextColor,
+      fontWeight: FontWeight.bold,
+    ),
+  );
+  static final textStyle16W400 = GoogleFonts.lato(
+    textStyle: TextStyle(
+      fontSize: 16.sp,
+      color: AppColors.primaryTextColor,
+      fontWeight: FontWeight.w400,
+    ),
+  );
+  static final textStyle16W500 = GoogleFonts.lato(
+    textStyle: TextStyle(
+      fontSize: 16.sp,
+      color: AppColors.primaryTextColor,
+      fontWeight: FontWeight.w500,
+    ),
+  );
+  static final textStyle16W600 = GoogleFonts.lato(
+    textStyle: TextStyle(
+      fontSize: 16.sp,
+      color: AppColors.primaryTextColor,
+      fontWeight: FontWeight.w600,
+    ),
+  );
+  static final textStyle18W300 = GoogleFonts.lato(
+    textStyle: TextStyle(
+      fontSize: 18.sp,
+      color: AppColors.primaryTextColor,
+      fontWeight: FontWeight.w300,
+    ),
+  );
+  static final textStyle18W500 = GoogleFonts.lato(
+    textStyle: TextStyle(
+      fontSize: 18.sp,
+      color: AppColors.primaryTextColor,
+      fontWeight: FontWeight.w500,
+    ),
+  );
+  static final textStyle18W600 = GoogleFonts.lato(
+    textStyle: TextStyle(
+      fontSize: 18.sp,
+      color: AppColors.primaryTextColor,
+      fontWeight: FontWeight.w600,
+    ),
+  );
+  static final textStyle20W500 = GoogleFonts.lato(
+    textStyle: TextStyle(
+      fontSize: 20.sp,
+      color: AppColors.primaryTextColor,
+      fontWeight: FontWeight.w500,
+    ),
+  );
+  static final textStyle20W600 = GoogleFonts.lato(
+    textStyle: TextStyle(
+      fontSize: 20.sp,
+      color: AppColors.primaryTextColor,
+      fontWeight: FontWeight.w600,
+    ),
+  );
+  static final textStyle22W500 = GoogleFonts.lato(
+    textStyle: TextStyle(
+      fontSize: 22.sp,
+      color: AppColors.primaryTextColor,
+      fontWeight: FontWeight.w500,
+    ),
+  );
+  static final textStyle22W600 = GoogleFonts.lato(
+    textStyle: TextStyle(
+      fontSize: 22.sp,
+      color: AppColors.primaryTextColor,
+      fontWeight: FontWeight.w600,
+    ),
+  );
+  static final textStyle22W700 = GoogleFonts.lato(
+    textStyle: TextStyle(
+      fontSize: 22.sp,
+      color: AppColors.primaryTextColor,
+      fontWeight: FontWeight.w700,
+    ),
+  );
+  static final textStyle26W600 = GoogleFonts.lato(
+    textStyle: TextStyle(
+      fontSize: 26.sp,
+      color: AppColors.primaryTextColor,
+      fontWeight: FontWeight.w600,
+    ),
+  );
+  static final textStyle28W600 = GoogleFonts.lato(
+    textStyle: TextStyle(
+      fontSize: 28.sp,
+      color: AppColors.primaryTextColor,
+      fontWeight: FontWeight.w600,
+    ),
+  );
+  static final textStyle30W300 = GoogleFonts.lato(
+    textStyle: TextStyle(
+      fontSize: 30.sp,
+      color: AppColors.primaryTextColor,
+      fontWeight: FontWeight.w300,
+    ),
+  );
+  static final textStyle32W700 = GoogleFonts.lato(
+    textStyle: TextStyle(
+      fontSize: 32.sp,
+      color: AppColors.primaryTextColor,
+      fontWeight: FontWeight.w700,
+    ),
+  );
+
+  static final textStyle18 = GoogleFonts.lato(
+    textStyle: TextStyle(
+      fontSize: 18.sp,
+      color: AppColors.primaryTextColor,
+      fontWeight: FontWeight.w600,
+    ),
+  );
+
+  static final textStyle20 = GoogleFonts.lato(
+    textStyle: TextStyle(
+      fontSize: 20.sp,
+      color: AppColors.primaryTextColor,
+      fontWeight: FontWeight.normal,
+    ),
+  );
+
+  static final textStyle22 = GoogleFonts.lato(
+    textStyle: TextStyle(
+      fontSize: 22.sp,
+      color: AppColors.primaryTextColor,
+      fontWeight: FontWeight.w500,
+    ),
+  );
+
+  static final textStyle24W500 = GoogleFonts.lato(
+    textStyle: TextStyle(
+      fontSize: 24.sp,
+      color: AppColors.primaryTextColor,
+      fontWeight: FontWeight.w500,
+      letterSpacing: 1.2,
+    ),
+  );
+
+  static final textStyle26 = GoogleFonts.lato(
+    textStyle: TextStyle(
+      fontSize: 26.sp,
+      color: AppColors.primaryTextColor,
+      fontWeight: FontWeight.w900,
+      letterSpacing: 1.2,
+    ),
+  );
+
+  static final textStyle30 = GoogleFonts.lato(
+    textStyle: TextStyle(
+      fontSize: 30.sp,
+      color: AppColors.primaryTextColor,
+      fontWeight: FontWeight.w900,
+      letterSpacing: 1.2,
+    ),
+  );
+
+  static final textStyle14 = GoogleFonts.lato(
+    textStyle: TextStyle(
+      fontSize: 14.sp,
+      color: AppColors.primaryTextColor,
+      fontWeight: FontWeight.normal,
+    ),
+  );
+
+  static final textStyle16 = GoogleFonts.lato(
+    textStyle: TextStyle(
+      fontSize: 16.sp,
+      color: AppColors.primaryTextColor,
+      fontWeight: FontWeight.w500,
+    ),
+  );
+  static final textStyle64W400 = GoogleFonts.lato(
+    textStyle: TextStyle(
+      fontSize: 64.sp,
+      color: AppColors.primaryTextColor,
+      fontWeight: FontWeight.w400,
+    ),
+  );
+}
