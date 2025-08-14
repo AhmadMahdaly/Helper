f823d71b2b31af7391dac3e5f66dab9e5b3ae792
Ahmed Mahmoud <ahmed.mahdaly86@gmail.com>
2025-08-14 13:00:51 +0300

    Update dio_factory.dart

--- core/networking/dio_factory.dart
+++ core/networking/dio_factory.dart
@@ -1,51 +1,32 @@
 import 'package:dio/dio.dart';
-import 'package:hr_app/core/cache_helper/cache_helper.dart';
-import 'package:hr_app/core/cache_helper/cache_values.dart';
-import 'package:hr_app/core/networking/end_points.dart';
-import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+import 'package:.../core/networking/dio_interceptors.dart';
+import 'package:.../core/networking/end_points.dart';
+import 'package:.../core/services/connectivity_service.dart';
 
 class DioFactory {
-  static Dio dio = Dio();
-
-  static Future<void> init() {
-    BaseOptions baseOptions = BaseOptions(
-      baseUrl: EndPoints.baseUrl,
-      receiveDataWhenStatusError: true,
-      validateStatus: (status) => true,
-    );
-
-    dio = Dio(baseOptions);
-    addDioInterceptor();
-    return Future.value();
-  }
-
-  Future<Response?> get({required String endPoint, data}) async {
-    dio.options.headers = {
-      "Accept": "application/json",
-      "lang": CacheHelper.getCurrentLanguage().toString(),
-      "Authorization": "Bearer ${CacheHelper.getData(key: 'token')}",
-    };
-    return await dio.get(endPoint, queryParameters: data);
-  }
-
-  Future<Response?> post({required String endPoint, data}) async {
-    dio.options.headers = {
-      "Accept": "application/json",
-      "lang": CacheHelper.getCurrentLanguage().toString(),
-      "Authorization":
-          "Bearer ${await CacheHelper.getSecuredString(key: CacheKeys.userToken)}",
-    };
-    return await dio.post(endPoint, data: data);
-  }
-
-  static void addDioInterceptor() {
-    dio.interceptors.add(
-      PrettyDioLogger(
-        requestBody: true,
-        requestHeader: true,
-        responseHeader: true,
-        responseBody: true,
+  DioFactory(this._connectivityService);
+  final ConnectivityService _connectivityService;
+  Dio createDio() {
+    final dio = Dio(
+      BaseOptions(
+        baseUrl: EndPoints.baseUrl,
+        receiveDataWhenStatusError: true,
+        connectTimeout: const Duration(seconds: 20),
+        receiveTimeout: const Duration(seconds: 20),
+        sendTimeout: const Duration(seconds: 20),
+        headers: {'Accept': 'application/json'},
       ),
     );
+
+    final dioInterceptors = DioInterceptors(dio, _connectivityService);
+
+    dio.interceptors.addAll([
+      // dioInterceptors.connectivityInterceptor(),
+      dioInterceptors.authInterceptor(),
+      dioInterceptors.languageInterceptor(),
+      dioInterceptors.debugeDioLogger(),
+    ]);
+
+    return dio;
   }
 }
