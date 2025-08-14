55c142e38041d02fae8719369424163905f7c30d
Ahmed Mahmoud <ahmed.mahdaly86@gmail.com>
2025-08-14 13:31:34 +0300

    Update and rename colors.dart to app_colors.dart

---
+++ core/theming/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryColor = Color(0xff3085fe);
+  static const secondaryColor = Color(0xffd5d5d5);
+  static const thirdColor = Color(0xff22444d);
+  static const forthColor = Color(0xff0e0f24);
+}
