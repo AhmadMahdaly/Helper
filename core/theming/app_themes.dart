999a534a98377bfd21eec1a6a5ac0f237f0c7874
Ahmed Mahmoud <ahmed.mahdaly86@gmail.com>
2025-08-14 13:31:04 +0300

    Update and rename text_style.dart to app_themes.dart

---
+++ core/theming/app_themes.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:opration/core/constants.dart';
+import 'package:opration/core/responsive/responsive_config.dart';
+import 'package:opration/core/theme/colors.dart';
+import 'package:opration/core/theme/text_style.dart';
+
+class Appthemes {
+  static ThemeData lightTheme() {
+    return ThemeData(
+      colorScheme: ColorScheme.fromSeed(seedColor: AppColors.blueLightColor),
+      useMaterial3: true,
+      primaryColor: AppColors.blueLightColor,
+      scaffoldBackgroundColor: AppColors.scaffoldBackgroundLightColor,
+      fontFamily: kPrimaryFont,
+
+      textTheme: TextTheme(
+        titleLarge: AppTextStyles.style18W800,
+        titleMedium: AppTextStyles.style16W500,
+      ),
+      appBarTheme: AppBarTheme(
+        iconTheme: const IconThemeData(
+          color: AppColors.scaffoldBackgroundLightColor,
+        ),
+        toolbarHeight: 100.h,
+        titleTextStyle: AppTextStyles.style18Bold.copyWith(
+          color: AppColors.scaffoldBackgroundLightColor,
+        ),
+        backgroundColor: AppColors.blueLightColor,
+        surfaceTintColor: AppColors.scaffoldBackgroundLightColor,
+      ),
+      cardTheme: CardThemeData(
+        elevation: 2,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15.r),
+        ),
+      ),
+      inputDecorationTheme: InputDecorationTheme(
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(15.r)),
+      ),
+      visualDensity: VisualDensity.adaptivePlatformDensity,
+
+      /// Dialog theme
+      dialogTheme: DialogThemeData(
+        backgroundColor: AppColors.scaffoldBackgroundLightColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20.r),
+        ),
+        elevation: 5,
+        titleTextStyle: AppTextStyles.style20Bold,
+      ),
+
+      /// ستايل الزر الرئيسي (ElevatedButton)
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: AppColors.blueLightColor,
+          foregroundColor:
+              AppColors.scaffoldBackgroundLightColor, // لون النص والأيقونة
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.r),
+          ),
+          padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 12.h),
+          textStyle: AppTextStyles.style12W500,
+        ),
+      ),
+
+      /// ستايل الزر الثانوي (TextButton)
+      textButtonTheme: TextButtonThemeData(
+        style: TextButton.styleFrom(
+          foregroundColor: AppColors.secondaryColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.r),
+          ),
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+          textStyle: AppTextStyles.style14W500,
+        ),
+      ),
+    );
+  }
+}
