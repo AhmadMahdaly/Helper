59c7eef6280b7c11bc970df5258cdd70a09f62fb
Ahmed Mahmoud <ahmed.mahdaly86@gmail.com>
2025-08-14 13:35:13 +0300

    Create custom_primary_button.dart

---
+++ core/shared_widgets/custom_primary_button.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:opration/core/responsive/responsive_config.dart';
+import 'package:.../core/theme/colors.dart';
+import 'package:.../core/theme/text_style.dart';
+
+class CustomPrimaryButton extends StatelessWidget {
+  const CustomPrimaryButton({
+    required this.text,
+    super.key,
+    this.onPressed,
+    this.width,
+  });
+  final void Function()? onPressed;
+  final String text;
+  final double? width;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: onPressed,
+        style: ButtonStyle(
+          backgroundColor: WidgetStateProperty.all(AppColors.blueLightColor),
+          tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+          minimumSize: WidgetStateProperty.all(Size(width ?? 300.w, 52.h)),
+          shape: WidgetStateProperty.all(
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.r)),
+          ),
+        ),
+        child: Text(
+          text,
+          style: AppTextStyles.style16W700.copyWith(
+            color: AppColors.scaffoldBackgroundLightColor,
+          ),
+        ),
+      ),
+    );
+  }
+}
