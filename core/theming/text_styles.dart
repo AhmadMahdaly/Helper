a4475977b2d165b8bc876b68be1812ecb74fb99a
Ahmed Mahmoud <ahmed.mahdaly86@gmail.com>
2025-08-14 13:29:42 +0300

    Update text_styles.dart

--- core/theming/text_styles.dart
+++ core/theming/text_styles.dart
@@ -1,264 +1,208 @@
-import 'package:hr_app/core/theming/colors.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:.../core/responsive/responsive_config.dart';
 
-abstract class Styles {
-  static final textStyle9W400 = GoogleFonts.lato(
-    textStyle: TextStyle(
-      fontSize: 9.sp,
-      color: AppColors.primaryTextColor,
-      fontWeight: FontWeight.w400,
-    ),
+abstract class AppTextStyles {
+  // --- Font Size 9 ---
+  static final TextStyle style9W300 = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 9.sp, tablet: 11.sp),
+    fontWeight: FontWeight.w300,
   );
-  static final textStyle10W400 = GoogleFonts.lato(
-    textStyle: TextStyle(
-      fontSize: 10.sp,
-      color: AppColors.primaryTextColor,
-      fontWeight: FontWeight.w400,
-    ),
+  static final TextStyle style9W400 = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 9.sp, tablet: 11.sp),
+    fontWeight: FontWeight.w400,
   );
-  static final textStyle12W400 = GoogleFonts.lato(
-    textStyle: TextStyle(
-      fontSize: 12.sp,
-      color: AppColors.primaryTextColor,
-      fontWeight: FontWeight.w400,
-    ),
+  static final TextStyle style9W500 = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 9.sp, tablet: 11.sp),
+    fontWeight: FontWeight.w500,
   );
-  static final textStyle12W700 = GoogleFonts.lato(
-    textStyle: TextStyle(
-      fontSize: 12.sp,
-      color: AppColors.primaryTextColor,
-      fontWeight: FontWeight.w700,
-    ),
+  static final TextStyle style9W600 = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 9.sp, tablet: 11.sp),
+    fontWeight: FontWeight.w600,
   );
-  static final textStyle13W500 = GoogleFonts.lato(
-    textStyle: TextStyle(
-      fontSize: 13.sp,
-      color: AppColors.primaryTextColor,
-      fontWeight: FontWeight.w500,
-    ),
+  static final TextStyle style9W700 = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 9.sp, tablet: 11.sp),
+    fontWeight: FontWeight.w700,
+  );
+  static final TextStyle style9W800 = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 9.sp, tablet: 11.sp),
+    fontWeight: FontWeight.w800,
+  );
+  static final TextStyle style9W900 = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 9.sp, tablet: 11.sp),
+    fontWeight: FontWeight.w900,
+  );
+  static final TextStyle style9Bold = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 9.sp, tablet: 11.sp),
+    fontWeight: FontWeight.bold,
   );
 
-  static final textStyle14W300 = GoogleFonts.lato(
-    textStyle: TextStyle(
-      fontSize: 14.sp,
-      color: AppColors.primaryTextColor,
-      fontWeight: FontWeight.w300,
-    ),
+  // --- Font Size 12 ---
+  static final TextStyle style12W300 = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 12.sp, tablet: 14.sp),
+    fontWeight: FontWeight.w300,
   );
-  static final textStyle14W400 = GoogleFonts.lato(
-    textStyle: TextStyle(
-      fontSize: 14.sp,
-      color: AppColors.primaryTextColor,
-      fontWeight: FontWeight.w400,
-    ),
+  static final TextStyle style12W400 = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 12.sp, tablet: 14.sp),
+    fontWeight: FontWeight.w400,
   );
-  static final textStyle14W700 = GoogleFonts.lato(
-    textStyle: TextStyle(
-      fontSize: 12.sp,
-      fontWeight: FontWeight.w700,
-      color: AppColors.primaryTextColor,
-    ),
+  static final TextStyle style12W500 = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 12.sp, tablet: 14.sp),
+    fontWeight: FontWeight.w500,
   );
-  static final textStyle14W600 = GoogleFonts.lato(
-    textStyle: TextStyle(
-      fontSize: 14.sp,
-      color: AppColors.primaryTextColor,
-      fontWeight: FontWeight.w600,
-    ),
+  static final TextStyle style12W600 = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 12.sp, tablet: 14.sp),
+    fontWeight: FontWeight.w600,
   );
-
-  static final textStyle15W300 = GoogleFonts.lato(
-    textStyle: TextStyle(
-      fontSize: 15.sp,
-      color: AppColors.primaryTextColor,
-      fontWeight: FontWeight.w300,
-    ),
+  static final TextStyle style12W700 = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 12.sp, tablet: 14.sp),
+    fontWeight: FontWeight.w700,
   );
-  static final textStyle15W700 = GoogleFonts.lato(
-    textStyle: TextStyle(
-      fontSize: 15.sp,
-      color: AppColors.primaryTextColor,
-      fontWeight: FontWeight.bold,
-    ),
+  static final TextStyle style12W800 = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 12.sp, tablet: 14.sp),
+    fontWeight: FontWeight.w800,
   );
-  static final textStyle16W400 = GoogleFonts.lato(
-    textStyle: TextStyle(
-      fontSize: 16.sp,
-      color: AppColors.primaryTextColor,
-      fontWeight: FontWeight.w400,
-    ),
+  static final TextStyle style12W900 = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 12.sp, tablet: 14.sp),
+    fontWeight: FontWeight.w900,
   );
-  static final textStyle16W500 = GoogleFonts.lato(
-    textStyle: TextStyle(
-      fontSize: 16.sp,
-      color: AppColors.primaryTextColor,
-      fontWeight: FontWeight.w500,
-    ),
-  );
-  static final textStyle16W600 = GoogleFonts.lato(
-    textStyle: TextStyle(
-      fontSize: 16.sp,
-      color: AppColors.primaryTextColor,
-      fontWeight: FontWeight.w600,
-    ),
-  );
-  static final textStyle18W300 = GoogleFonts.lato(
-    textStyle: TextStyle(
-      fontSize: 18.sp,
-      color: AppColors.primaryTextColor,
-      fontWeight: FontWeight.w300,
-    ),
-  );
-  static final textStyle18W500 = GoogleFonts.lato(
-    textStyle: TextStyle(
-      fontSize: 18.sp,
-      color: AppColors.primaryTextColor,
-      fontWeight: FontWeight.w500,
-    ),
-  );
-  static final textStyle18W600 = GoogleFonts.lato(
-    textStyle: TextStyle(
-      fontSize: 18.sp,
-      color: AppColors.primaryTextColor,
-      fontWeight: FontWeight.w600,
-    ),
-  );
-  static final textStyle20W500 = GoogleFonts.lato(
-    textStyle: TextStyle(
-      fontSize: 20.sp,
-      color: AppColors.primaryTextColor,
-      fontWeight: FontWeight.w500,
-    ),
-  );
-  static final textStyle20W600 = GoogleFonts.lato(
-    textStyle: TextStyle(
-      fontSize: 20.sp,
-      color: AppColors.primaryTextColor,
-      fontWeight: FontWeight.w600,
-    ),
-  );
-  static final textStyle22W500 = GoogleFonts.lato(
-    textStyle: TextStyle(
-      fontSize: 22.sp,
-      color: AppColors.primaryTextColor,
-      fontWeight: FontWeight.w500,
-    ),
-  );
-  static final textStyle22W600 = GoogleFonts.lato(
-    textStyle: TextStyle(
-      fontSize: 22.sp,
-      color: AppColors.primaryTextColor,
-      fontWeight: FontWeight.w600,
-    ),
-  );
-  static final textStyle22W700 = GoogleFonts.lato(
-    textStyle: TextStyle(
-      fontSize: 22.sp,
-      color: AppColors.primaryTextColor,
-      fontWeight: FontWeight.w700,
-    ),
-  );
-  static final textStyle26W600 = GoogleFonts.lato(
-    textStyle: TextStyle(
-      fontSize: 26.sp,
-      color: AppColors.primaryTextColor,
-      fontWeight: FontWeight.w600,
-    ),
-  );
-  static final textStyle28W600 = GoogleFonts.lato(
-    textStyle: TextStyle(
-      fontSize: 28.sp,
-      color: AppColors.primaryTextColor,
-      fontWeight: FontWeight.w600,
-    ),
-  );
-  static final textStyle30W300 = GoogleFonts.lato(
-    textStyle: TextStyle(
-      fontSize: 30.sp,
-      color: AppColors.primaryTextColor,
-      fontWeight: FontWeight.w300,
-    ),
-  );
-  static final textStyle32W700 = GoogleFonts.lato(
-    textStyle: TextStyle(
-      fontSize: 32.sp,
-      color: AppColors.primaryTextColor,
-      fontWeight: FontWeight.w700,
-    ),
+  static final TextStyle style12Bold = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 12.sp, tablet: 14.sp),
+    fontWeight: FontWeight.bold,
   );
 
-  static final textStyle18 = GoogleFonts.lato(
-    textStyle: TextStyle(
-      fontSize: 18.sp,
-      color: AppColors.primaryTextColor,
-      fontWeight: FontWeight.w600,
-    ),
+  // --- Font Size 14 ---
+  static final TextStyle style14W300 = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 14.sp, tablet: 16.sp),
+    fontWeight: FontWeight.w300,
+  );
+  static final TextStyle style14W400 = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 14.sp, tablet: 16.sp),
+    fontWeight: FontWeight.w400,
+  );
+  static final TextStyle style14W500 = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 14.sp, tablet: 16.sp),
+    fontWeight: FontWeight.w500,
+  );
+  static final TextStyle style14W600 = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 14.sp, tablet: 16.sp),
+    fontWeight: FontWeight.w600,
+  );
+  static final TextStyle style14W700 = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 14.sp, tablet: 16.sp),
+    fontWeight: FontWeight.w700,
+  );
+  static final TextStyle style14W800 = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 14.sp, tablet: 16.sp),
+    fontWeight: FontWeight.w800,
+  );
+  static final TextStyle style14W900 = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 14.sp, tablet: 16.sp),
+    fontWeight: FontWeight.w900,
+  );
+  static final TextStyle style14Bold = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 14.sp, tablet: 16.sp),
+    fontWeight: FontWeight.bold,
   );
 
-  static final textStyle20 = GoogleFonts.lato(
-    textStyle: TextStyle(
-      fontSize: 20.sp,
-      color: AppColors.primaryTextColor,
-      fontWeight: FontWeight.normal,
-    ),
+  // --- Font Size 16 ---
+  static final TextStyle style16W300 = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 16.sp, tablet: 18.sp),
+    fontWeight: FontWeight.w300,
+  );
+  static final TextStyle style16W400 = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 16.sp, tablet: 18.sp),
+    fontWeight: FontWeight.w400,
+  );
+  static final TextStyle style16W500 = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 16.sp, tablet: 18.sp),
+    fontWeight: FontWeight.w500,
+  );
+  static final TextStyle style16W600 = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 16.sp, tablet: 18.sp),
+    fontWeight: FontWeight.w600,
+  );
+  static final TextStyle style16W700 = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 16.sp, tablet: 18.sp),
+    fontWeight: FontWeight.w700,
+  );
+  static final TextStyle style16W800 = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 16.sp, tablet: 18.sp),
+    fontWeight: FontWeight.w800,
+  );
+  static final TextStyle style16W900 = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 16.sp, tablet: 18.sp),
+    fontWeight: FontWeight.w900,
+  );
+  static final TextStyle style16Bold = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 16.sp, tablet: 18.sp),
+    fontWeight: FontWeight.bold,
   );
 
-  static final textStyle22 = GoogleFonts.lato(
-    textStyle: TextStyle(
-      fontSize: 22.sp,
-      color: AppColors.primaryTextColor,
-      fontWeight: FontWeight.w500,
-    ),
+  // --- Font Size 18 ---
+  static final TextStyle style18W300 = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 18.sp, tablet: 20.sp),
+    fontWeight: FontWeight.w300,
+  );
+  static final TextStyle style18W400 = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 18.sp, tablet: 20.sp),
+    fontWeight: FontWeight.w400,
+  );
+  static final TextStyle style18W500 = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 18.sp, tablet: 20.sp),
+    fontWeight: FontWeight.w500,
+  );
+  static final TextStyle style18W600 = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 18.sp, tablet: 20.sp),
+    fontWeight: FontWeight.w600,
+  );
+  static final TextStyle style18W700 = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 18.sp, tablet: 20.sp),
+    fontWeight: FontWeight.w700,
+  );
+  static final TextStyle style18W800 = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 18.sp, tablet: 20.sp),
+    fontWeight: FontWeight.w800,
+  );
+  static final TextStyle style18W900 = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 18.sp, tablet: 20.sp),
+    fontWeight: FontWeight.w900,
+  );
+  static final TextStyle style18Bold = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 18.sp, tablet: 20.sp),
+    fontWeight: FontWeight.bold,
   );
 
-  static final textStyle24W500 = GoogleFonts.lato(
-    textStyle: TextStyle(
-      fontSize: 24.sp,
-      color: AppColors.primaryTextColor,
-      fontWeight: FontWeight.w500,
-      letterSpacing: 1.2,
-    ),
+  // --- Font Size 20 ---
+  static final TextStyle style20W300 = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 20.sp, tablet: 22.sp),
+    fontWeight: FontWeight.w300,
   );
-
-  static final textStyle26 = GoogleFonts.lato(
-    textStyle: TextStyle(
-      fontSize: 26.sp,
-      color: AppColors.primaryTextColor,
-      fontWeight: FontWeight.w900,
-      letterSpacing: 1.2,
-    ),
+  static final TextStyle style20W400 = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 20.sp, tablet: 22.sp),
+    fontWeight: FontWeight.w400,
   );
-
-  static final textStyle30 = GoogleFonts.lato(
-    textStyle: TextStyle(
-      fontSize: 30.sp,
-      color: AppColors.primaryTextColor,
-      fontWeight: FontWeight.w900,
-      letterSpacing: 1.2,
-    ),
+  static final TextStyle style20W500 = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 20.sp, tablet: 22.sp),
+    fontWeight: FontWeight.w500,
   );
-
-  static final textStyle14 = GoogleFonts.lato(
-    textStyle: TextStyle(
-      fontSize: 14.sp,
-      color: AppColors.primaryTextColor,
-      fontWeight: FontWeight.normal,
-    ),
+  static final TextStyle style20W600 = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 20.sp, tablet: 22.sp),
+    fontWeight: FontWeight.w600,
   );
-
-  static final textStyle16 = GoogleFonts.lato(
-    textStyle: TextStyle(
-      fontSize: 16.sp,
-      color: AppColors.primaryTextColor,
-      fontWeight: FontWeight.w500,
-    ),
+  static final TextStyle style20W700 = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 20.sp, tablet: 22.sp),
+    fontWeight: FontWeight.w700,
   );
-  static final textStyle64W400 = GoogleFonts.lato(
-    textStyle: TextStyle(
-      fontSize: 64.sp,
-      color: AppColors.primaryTextColor,
-      fontWeight: FontWeight.w400,
-    ),
+  static final TextStyle style20W800 = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 20.sp, tablet: 22.sp),
+    fontWeight: FontWeight.w800,
+  );
+  static final TextStyle style20W900 = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 20.sp, tablet: 22.sp),
+    fontWeight: FontWeight.w900,
+  );
+  static final TextStyle style20Bold = TextStyle(
+    fontSize: SizeConfig.responsiveValue(phone: 20.sp, tablet: 22.sp),
+    fontWeight: FontWeight.bold,
   );
 }
